99d5bb2277b40ab618ac4756916c8d254abc45b1
jeffreyrene <jeffreyrene575@gmail.com>
2024-12-03 00:00:00 +0500

    feat: update Explorer.module

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import type { AppProps } from "next/app";
+
+import Layout from "@/components/Layout";
+import Head from "@/components/Head";
+
+import "@/styles/globals.css";
+import "@/styles/themes.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme) {
+      document.documentElement.setAttribute("data-theme", theme);
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <Head title={`Jeffrey Rene | ${pageProps.title}`} />
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
+
+# touched on 2025-06-13T17:42:12.926709Z
